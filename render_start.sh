3b50832ef51a3a93e967d9355456a5c9582226b7
Matiaslagost7 <matiaslagost7@gmail.com>
2025-11-24 18:22:22 -0300

    Agrega collectstatic al script de inicio para servir archivos estáticos en producción

--- render_start.sh
+++ render_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script para ejecutar migraciones y crear superusuario si no existen usuarios
+# Script para ejecutar migraciones, collectstatic y crear superusuario si no existen usuarios
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 python manage.py createsuperifnone < /dev/null
 exec gunicorn portafolio.wsgi:application
